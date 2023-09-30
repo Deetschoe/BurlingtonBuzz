c911d22532e0930fe07ba0f7f4f298c0e6311796
Deet <48701612+Deetschoe@users.noreply.github.com>
2023-09-30 12:40:20 -0400

    fixed gitingore and next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const withPWA = require ("next-pwa");
 
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = withPWA({
+reactStrictMode: true,
+pwa: {
+dest: "public",
+ register: true, 
+ skipWaiting: true,
+// disable: process.env.NODE_ENV ===
 }
-module.exports = nextConfig
+});
