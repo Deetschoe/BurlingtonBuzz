7901f9ee17bde0fd591b84e91631b4557db92d32
Deet <48701612+Deetschoe@users.noreply.github.com>
2023-09-29 22:56:36 -0400

    mm yes no changes

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,6 @@
 import { useState, useEffect } from 'react';
-import 'tiny-slider/dist/tiny-slider.css';
-// import { tns } from "tiny-slider/src/tiny-slider";
 
 function MyComponent() {
-  const [isClient, setIsClient] = useState(false);
-
-  // useEffect(() => {
-  //   setIsClient(true);
-
-  //   if (isClient) {
-  //     // Create the h1 element
-  //     const titleElement = document.createElement('h1');
-  //     titleElement.textContent = "Burlington Buzz";
-
-  //     // Apply inline styles
-  //     titleElement.style.fontFamily = 'Arial, sans-serif';
-  //     titleElement.style.fontSize = '32px';
-  //     titleElement.style.color = '#333';
-  //     titleElement.style.textAlign = 'center';
-  //     titleElement.style.padding = '20px';
-  //     titleElement.style.backgroundColor = '#f5f5f5';
-
-  //     // Append the title to the placeholder in the HTML
-  //     const placeholder = document.getElementById('titlePlaceholder');
-  //     if (placeholder) {
-  //       placeholder.appendChild(titleElement);
-  //     }
-  //   }
-  // }, [isClient]);  // Runs whenever isClient changes
 
   return (
     <div>
@@ -44,15 +17,13 @@ function MyComponent() {
                 <img src="https://cloud-9lq65xru1-hack-club-bot.vercel.app/0white-paper__2_.jpg" alt="Description 2" />
                 <p className="slide-text">Paper texture yes mmm</p>
             </div>
-            {/* Add more slides as needed */}
         </div>
-        <button id="prev" className="controls">
-            <img src="/back.svg" alt="Previous Slide" />
+        <button id="prev" className="controls" onClick={() => console.log("Prev clicked!")}>
+            <img src="/back.svg" alt="Previous Slide" style={{pointerEvents: "none"}} />
         </button>
-        <button id="next" className="controls">
-            <img src="/forward.svg" alt="Next Slide" />
+        <button id="next" className="controls" onClick={() => console.log("Next clicked!")}>
+            <img src="/forward.svg" alt="Next Slide" style={{pointerEvents: "none"}} />
         </button>
-        {/* ... other JSX ... */}
     </div>
   );
 }
